2363ec22dd1eae7297896898d8f0fe5c8cda0e99
Ahmad Maulana <ahmad.maulana@mekari.com>
2022-12-16 22:32:24 +0700

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Ahmad Maulana on 16/12/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
