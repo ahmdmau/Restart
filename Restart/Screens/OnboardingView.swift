ef414ce63adbf91c3a95fe46f9bf9312b9b92565
Ahmad Maulana <ahmad.maulana@mekari.com>
2022-12-18 18:29:49 +0700

    Add haptic feedback and sound

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -17,6 +17,8 @@ struct OnboardingView: View {
     @State private var indicatorOpacity: Double = 1.0
     @State private var textTitle = "Share."
     
+    let hapticFeedback = UINotificationFeedbackGenerator()
+    
     // MARK: - Body
     var body: some View {
         ZStack {
@@ -144,9 +146,12 @@ struct OnboardingView: View {
                                 .onEnded { _ in
                                     withAnimation(Animation.easeOut(duration: 0.4)) {
                                         if buttonOffset > buttonWidth / 2 {
+                                            hapticFeedback.notificationOccurred(.success)
+                                            playSound(sound: "chimeup", type: "mp3")
                                             buttonOffset = buttonWidth - 80
                                             isOnboardingViewActive = false
                                         } else {
+                                            hapticFeedback.notificationOccurred(.warning)
                                             buttonOffset = 0
                                         }
                                     }
@@ -164,7 +169,7 @@ struct OnboardingView: View {
             }
         }.onAppear {
             isAnimating = true
-        }
+        }.preferredColorScheme(.dark)
     }
 }
 
